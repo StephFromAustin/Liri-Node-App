df9a819a6996743a90de280eb7b6800cd15dfc2c
KSG Sounds Macbook Pro <ksgsound@KSGs-MBP.attlocal.net>
2018-10-11 22:33:17 -0500

    liri.js fixes, after class TA 10-11-18

--- liri.js
+++ liri.js
@@ -1,19 +1,26 @@
 // CODE REQUIRED FOR FILE SYSTEM
-let fs = required('fs');
+const fs = required('fs');
+const request = require('request');
+let Spotify = require('node-spotify-api');
 
 // CODE READS AND SETS ANY ENVIROMENT VARIABLES WITH THE dotenv PACKAGE. 
 require("dotenv").config();
 
 //CODE REQUIRED TO IMPORT THE keys.js FILE AND SOTRE IT IN A VARIABLE. 
-let dataKeys = require("keys.js");
+    // "./keys" IS THE BETTER WAY TO LOOK FOR IT BECAUSE .JS IS REDUNDANT 
+let keys = require("./keys");
 // let spotify =  require("node-spotify-api"); => needs to be within the SPOTIFY function, delete before final master push 
 
 // COMMAND AND INPUT FUNCTION 
-// let command = process.argv[2];
-// let input = process.argv[3];
+let command = process.argv[2]; 
+let input = process.argv[3];
 
 //SPOTIFY FUNCTION: make it so liri.js can take inone of the follow commands: (spotify) => spotify-this-song
 
+// PULLING KEYS TO SET UP CONSTRUCTOR 
+let spotify = new Spotify(keys.spotify);
+
+//SPOTIFY FUNCTION 
 let spotifyThisSong= function(songName){
     // LOAD SPOTIFY NPM PACKAGE (LOADED!!!)
     let spotify = require('spotify');
@@ -23,6 +30,6 @@ let spotifyThisSong= function(songName){
     }
 
     // SPOTIFY SEARCH 
-    spotify.search 
+    // spotify.search 
 
 }
